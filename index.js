afb86179b6eb6ce5d791a783f540873ba20cbae4
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-04-27 13:34:51 +0100

    touch screen responsive

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
-
 let gamePattern = [];
 let userClickedPattern = [];
-
 let started = false;
 let level = 0;
 
-$(document).keydown(function () {
+$(document).keydown()||touchstart(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -17,32 +15,26 @@ $(document).keydown(function () {
 $(".btn").click(function () {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-   
-  if (userClickedPattern.length === gamePattern.length) {
-    setTimeout(() => {
-      nextSequence();
-    }, 1000);
-}
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
   } else {
-    playSound("wrong")
-    $("body").addClass("game-over")
-
-      $("#level-title").text("Game over, Press Any Key to Restart")
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game over, Press Any Key to Restart");
     setTimeout(() => {
-        $("body").removeClass("game-over")
+      $("body").removeClass("game-over");
     }, 200);
-  
-    
-    startOver()
+    startOver();
   }
 }
 
@@ -50,12 +42,12 @@ function nextSequence() {
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
-
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
   playSound(randomChosenColour);
 }
 
@@ -67,16 +59,12 @@ function animatePress(currentColour) {
 }
 
 function playSound(name) {
-
-    let audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-  }
-
-
-
-function startOver() {
-    level = 0
-    gamePattern =[]
-    started = false
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
